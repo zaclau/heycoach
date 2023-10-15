46b43120ec52efa2f4b0782caba8759e2fe53f6b
zaclau <59416079+zaclau@users.noreply.github.com>
2023-10-15 23:43:36 +0800

    Add logo to navbar

--- hey_coach/src/components/navbar/Navbar.jsx
+++ hey_coach/src/components/navbar/Navbar.jsx
@@ -18,7 +18,14 @@ const Navbar = () => {
   return (
     <div className="navbar navbar-expand justify-content-center">
         <div className="container-xxl bd-gutter">
-            <div className="navbar-brand fw-bold" onClick={ goHome }>HeyCoach!</div>
+            <a className="navbar-brand fw-bold" onClick={ goHome }>
+              <img 
+                  src="https://cdn.icon-icons.com/icons2/2172/PNG/512/spring_sprout_plant_garden_growth_leaves_seedling_icon_133303.png"
+                  width="25px"
+                  className="me-1 pe-1"
+              ></img>
+                HeyCoach!
+            </a>
             <div className="navItems">
                 <button className="btn btn-link link-dark fw-bold" onClick={ goLogin }>Login</button>
                 <button className="btn btn-link link-dark fw-bold" onClick={ goSignup }>Sign Up</button>
